b03871a36de4885db8ebde58fcac426c288035e0
Subhra Mukherjee <subhra.m@techmantra.co>
2026-06-01 19:52:51 +0530

    push

--- src/CompanyInfo/CompanyInfo.js
+++ src/CompanyInfo/CompanyInfo.js
@@ -1,4 +1,4 @@
-export const companyName = "SALES DRIVE";
+export const companyName = "Calcutta Metal Corporation";
 export const companyLogo =
   "https://firebasestorage.googleapis.com/v0/b/zoofi-6b109.firebasestorage.app/o/zoofi-marketplace%2Fzoofi_1746338266273.jpeg?alt=media";
 export const companyFavicon =
