6b3caaf74fce0ca1077b18cdcc1b8fa201fe8b81
Suvo Biswas <suvo@techmantra.co>
2026-08-12 17:28:39 +0530

    okkk

--- src/pages/admin/Designation.jsx
+++ src/pages/admin/Designation.jsx
@@ -402,15 +402,15 @@ const Designation = () => {
                   >
                     <option value="">Select Parent Designation</option>
                     {filteredDesignations
-  .filter(
-    (element) =>
-      element.name === "ASM" || element.name === "RSM"
-  )
-  .map((element) => (
-    <option key={element._id} value={element._id}>
-      {element.name}
-    </option>
-  ))}
+                      .filter(
+                        (element) =>
+                          element.name === "ASM" || element.name === "RSM"
+                      )
+                      .map((element) => (
+                        <option key={element._id} value={element._id}>
+                          {element.name}
+                        </option>
+                      ))}
                   </Select>
                 </div>
 
